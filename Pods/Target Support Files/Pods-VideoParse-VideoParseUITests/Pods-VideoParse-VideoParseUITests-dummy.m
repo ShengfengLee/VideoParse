f90307c42983f4c57fd313ef967dfd3cda11acae
小李霸王 <389933523@qq.com>
2020-11-05 09:54:54 +0800

    remove Pod/

--- Pods/Target Support Files/Pods-VideoParse-VideoParseUITests/Pods-VideoParse-VideoParseUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_VideoParse_VideoParseUITests : NSObject
-@end
-@implementation PodsDummy_Pods_VideoParse_VideoParseUITests
-@end
--- Pods/Target Support Files/Pods-VideoParseTests/Pods-VideoParseTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_VideoParseTests : NSObject
-@end
-@implementation PodsDummy_Pods_VideoParseTests
-@end
